5e52f0500876e8da816f7a0e084ada6b183df56c
Prakhar Bhandari <prakhar23.bhandari@gmail.com>
2023-08-24 01:19:42 -0400

    Updates

---
+++ precache-manifest.f0f698c08aa6475a10306d9ee76e86aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eac7d3e7402216ae9790",
+    "url": "/world-guesser/static/css/main.6dd007a3.chunk.css"
+  },
+  {
+    "revision": "eac7d3e7402216ae9790",
+    "url": "/world-guesser/static/js/main.eac7d3e7.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/world-guesser/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "04ccf6edfe5f5f55ea34",
+    "url": "/world-guesser/static/js/2.04ccf6ed.chunk.js"
+  },
+  {
+    "revision": "344258b6a488b7fcb428",
+    "url": "/world-guesser/static/js/runtime~main.344258b6.js"
+  },
+  {
+    "revision": "04f3dd7c1b84816c49d92d11a3fe4504",
+    "url": "/world-guesser/index.html"
+  }
+];
